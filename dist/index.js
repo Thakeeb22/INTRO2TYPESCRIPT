df9b9d4b38762e832468765e6969048322116b9e
devfoma <oluebube.madu@yahoo.com>
2025-11-13 03:00:55 -0500

    yeahh more exercises

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Hehe yes it's empty you have to fill it with your solutions
+var size;
+(function (size) {
+    size[size["small"] = 1] = "small";
+    size[size["medium"] = 2] = "medium";
+    size[size["large"] = 3] = "large";
+})(size || (size = {}));
+let mySize = size.medium;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
